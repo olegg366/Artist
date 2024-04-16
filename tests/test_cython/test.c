6953c78f627956ad326181febe180e5ae8dbe286
olegg366 <oleg.drokanov@yandex.ru>
2024-04-16 15:51:31 +0300

    Попробовал передачу 3D-массивов

--- tests/test_cython/test.c
+++ tests/test_cython/test.c
@@ -1,13 +1,14 @@
 #include <stdlib.h> 
 #include <stdio.h>
 
-void foobar(int m, int n, double **x) 
+void foobar(int m, int n, int l, double ***x) 
 { 
     for(int i = 0; i < m; i++) 
     {    
         for(int j = 0; j < n; j++) 
         {    
-            printf("%f ", x[i][j]);
+            for (int k = 0; k < l; k++)
+                printf("%f ", x[i][j][k]);
         }
         printf("\n");
     }
--- tests/test_cython/test.py
+++ tests/test_cython/test.py
@@ -8,19 +8,18 @@ path = 'D:\\Python_progs\\AI\\Artist\\tests\\test_cython\\'
 
 os.system(f'gcc -Wall -pedantic -shared -fPIC -o {path}lib.dll {path}test.c')
 
-doublepp = ndpointer(dtype=np.uintp, ndim=1, flags='C') 
+tpp = ndpointer(dtype=np.uintp, ndim=1, flags='C') 
 
 dll = ctypes.CDLL(path + 'lib.dll', winmode=0) 
 
 foobar = dll.foobar 
-foobar.argtypes = [ctypes.c_int, ctypes.c_int, doublepp] 
+foobar.argtypes = [ctypes.c_int, ctypes.c_int, tpp] 
 foobar.restype = None 
 
 def get_args(x: np.ndarray): 
     xpp = (x.__array_interface__['data'][0] + np.arange(x.shape[0]) * x.strides[0]).astype(np.uintp) 
-    m = ctypes.c_int(x.shape[0]) 
-    n = ctypes.c_int(x.shape[1]) 
-    return m, n, xpp
+    return *x.shape, xpp
 
-x = np.zeros([2, 2], dtype=np.double)
+x = np.zeros([2, 2, 2], dtype=np.double)
+print(get_args(x))
 foobar(*get_args(x))
